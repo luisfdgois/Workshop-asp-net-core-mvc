33cb58915edfed42c0639ef0c0ae8bcab4a4728a
Luis Fernando <luis.dantass@hotmail.com>
2019-05-21 11:47:00 -0300

    Criando outras classes e Segunda Migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,4 +1,8 @@
 ﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace SalesWebMvc.Models
 {
     public class Department
@@ -6,16 +10,27 @@ namespace SalesWebMvc.Models
 
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; }
 
         public Department()
         {
-
+            Sellers = new List<Seller>();
         }
 
-        public Department(int id, string name)
-        {
+        public Department(int id, string name) : this()
+        {  
             Id = id;
             Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(s => s.TotalSales(initial, final));
         }
     }
 }
